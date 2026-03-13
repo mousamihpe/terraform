2deda9720e216d8709d9a71174ec101f3ae175ae
mousamihpe <mousamichand2016@gmail.com>
2026-03-13 16:04:02 +0000

    Create variables.tf

---
+++ openstack/securitygroup/variables.tf
@@ -0,0 +1,8 @@
+variable "os_auth_url" {}
+variable "os_tenant" {}
+variable "os_username" {}
+variable "os_password" {}
+variable "os_region" {}
+variable "vm_flavor" { default = "m1.small" }
+variable "vm_image"  { default = "ubuntu-22.04" }
+variable "keypair"   {}
